16123f176a3f015933daef45adb5d606a7c39eb8
Stefanos Michelakis <stefanosmichelakis@gmail.com>
2023-03-31 18:57:15 +0300

    Add theming and material UI

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    styledComponents: true,
+  },
   reactStrictMode: true,
 };
 
---
+++ theme.ts
@@ -0,0 +1,51 @@
+/* eslint-disable sort-keys-fix/sort-keys-fix */
+import { amber, blue, grey, red } from '@mui/material/colors';
+
+export const colors = {
+  darkBlue: blue[800],
+  error: red[500],
+  secondary: amber[500],
+  textGray: grey[500],
+  textGrayDark: grey[800],
+  textGrayLight: grey[300],
+  white: grey[100],
+};
+
+export const spacing = {
+  xs: '4px',
+  sm: '8px',
+  md: '12px',
+  lg: '16px',
+  xl: '20px',
+  xxl: '24px',
+  xxxl: '32px',
+  xxxxl: '60px',
+};
+
+export const fontSize = {
+  xs: '12px',
+  sm: '14px',
+  md: '16px',
+  lg: '20px',
+  xl: '24px',
+  xxl: '28px',
+  xxxl: '32px',
+};
+
+export const borderRadius = {
+  sm: '5px',
+  md: '10px',
+  lg: '15px',
+};
+
+export const commonWeights = {
+  100: 100,
+  200: 200,
+  300: 300,
+  400: 400,
+  500: 500,
+  600: 600,
+  700: 700,
+  800: 800,
+  900: 900,
+};
