5b2d49119bcdf1bf66776f8ae5b2f60b3c59d0e6
Stefanos Michelakis <stefanosmichelakis@gmail.com>
2023-03-31 13:30:02 +0300

    Set up nextjs, eslint, typescript and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,109 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'react-native/react-native': true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'simple-import-sort',
+    'sort-keys-fix',
+    'typescript-sort-keys',
+    'sort-destructure-keys',
+    'react-native',
+  ],
+  root: true,
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/no-cycle': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-relative-packages': 'off',
+    'import/prefer-default-export': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        aspects: ['invalidHref', 'preferButton'],
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+      },
+    ],
+    'no-restricted-globals': 'off',
+    'no-shadow': 'off',
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    'prettier/prettier': [
+      'error',
+      { endOfLine: 'auto', singleQuote: true, trailingComma: 'es5' },
+    ],
+    'react-native/no-single-element-style-arrays': 2,
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/jsx-curly-newline': 'off',
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'sort-destructure-keys/sort-destructure-keys': [
+      1,
+      { caseSensitive: false },
+    ],
+    'sort-keys-fix/sort-keys-fix': 'warn',
+    'typescript-sort-keys/interface': 'warn',
+    'typescript-sort-keys/string-enum': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      typescript: {},
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+};
