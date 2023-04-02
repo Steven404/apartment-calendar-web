899829e3bab1881337eebe67309d0499fdbd054f
Stefanos Michelakis <stefanosmichelakis@gmail.com>
2023-04-02 22:04:08 +0300

    Fix next config and datepicker changes

--- components/datePicker/DatePicker.tsx
+++ components/datePicker/DatePicker.tsx
@@ -1,17 +1,36 @@
+import { DatePicker as MUIDatePicker } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import {
   StaticDatePicker,
   StaticDatePickerProps,
 } from '@mui/x-date-pickers/StaticDatePicker';
+import { Dayjs } from 'dayjs';
 import React from 'react';
 
-type DatePickerExtendedProps = StaticDatePickerProps<Date>;
+import { colors, spacing } from '@/theme';
 
-const DatePicker = ({ ...restProps }: DatePickerExtendedProps) => {
+// Extend the props from StaticDatePicker. The Dayjs package is required
+type DatePickerExtendedProps = StaticDatePickerProps<Dayjs> & {
+  isPhone?: boolean;
+};
+
+const DatePicker = ({ isPhone, ...restProps }: DatePickerExtendedProps) => {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <StaticDatePicker {...restProps} />
+      {isPhone ? (
+        <MUIDatePicker
+          sx={{
+            color: colors.darkBlue,
+            marginBottom: spacing.md,
+            marginTop: spacing.lg,
+            width: 250,
+          }}
+          {...restProps}
+        />
+      ) : (
+        <StaticDatePicker sx={{ color: colors.darkBlue }} {...restProps} />
+      )}
     </LocalizationProvider>
   );
 };
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
+  compiler: {
     styledComponents: true,
   },
   reactStrictMode: true,
